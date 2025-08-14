9f1543a3dda91eefede2076d2c93bcba8bb94c9d
NguyenTatHuan <159444791+NguyenTatHuan@users.noreply.github.com>
2025-08-14 21:45:55 +0700

    Initial commit

--- app.py
+++ app.py
@@ -3,19 +3,18 @@ from modules.prediction import get_market_chart
 
 app = Flask(__name__)
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    data = request.json
-    coin_id = data.get('coin_id')
-    predict_days = int(data.get('predict_days', 30))
-    currency = data.get('currency', 'usd')
+    if request.method == 'POST':
+        data = request.json
+        coin_id = data.get('coin_id')
+        predict_days = int(data.get('predict_days', 30))
+        currency = data.get('currency', 'usd')
 
-    predictions = get_market_chart(coin_id, predict_days, currency)
-    return jsonify({'coin_id': coin_id, 'currency': currency, 'predictions': predictions})
-
-@app.route('/health', methods=['GET'])
-def health_check():
-    return "OK", 200
+        predictions = get_market_chart(coin_id, predict_days, currency)
+        return jsonify({'coin_id': coin_id, 'currency': currency, 'predictions': predictions})
+    else:
+        return "OK", 200
 
 if __name__ == '__main__':
     app.run()
