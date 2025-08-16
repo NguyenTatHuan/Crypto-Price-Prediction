26634e2419698b56d732e85d222cf834bf224f13
NguyenTatHuan <159444791+NguyenTatHuan@users.noreply.github.com>
2025-08-16 15:05:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,14 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        data = request.json
-        coin_id = data.get('coin_id')
-        days = data.get('days', 30)
-        currency = data.get('currency', 'usd')
-
-        predictions = get_market_chart(coin_id, days, currency)
-        return jsonify({'coin_id': coin_id, 'currency': currency, 'predictions': predictions})
-    else:
-        return "OK", 200
+    data = request.json
+    coin_id = data.get('coin_id')
+    days = data.get('days', 30)
+    currency = data.get('currency', 'usd')
+    predictions = get_market_chart(coin_id, days, currency)
+    return jsonify({'coin_id': coin_id, 'currency': currency, 'predictions': predictions})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
